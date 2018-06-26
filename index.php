94c10f601c93dfef13acc2a2d4912282fe6d8ce1
gilianopiassa <gilianopiassa@gmail.com>
2018-06-25 21:09:31 -0300

    aula raintpl

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+// namespace
+use Rain\Tpl;
+// config
+$config = array(
+    "tpl_dir"       => "tpl/",
+    "cache_dir"     => "cache/",
+    "debug"         => false // set to false to improve the speed    
+);
+
+Tpl::configure( $config );
+
+// create the Tpl object
+$tpl = new Tpl;
+
+// assign a variable
+$tpl->assign( "name", "Giliano Piassa" );
+$tpl->assign( "version", PHP_VERSION );
+// assign an array
+
+// draw the template
+$tpl->draw( "index" );
+
+?>	
